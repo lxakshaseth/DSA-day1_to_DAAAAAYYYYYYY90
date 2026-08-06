150ec5cab0c3eacf81c82f487f9ee0d72e4b2cee
lxakshaseth <lxakshatseth90@gmail.com>
2026-08-06 14:18:03 +0530

    revsion the concept of print length

---
+++ String/length.java
@@ -0,0 +1,12 @@
+package String;
+
+public class length {
+    public static void main(String[] args) {
+        
+  
+         String fullName = "lxakshatseth";
+        System.out.println(fullName.length());
+          }
+    
+}
+
