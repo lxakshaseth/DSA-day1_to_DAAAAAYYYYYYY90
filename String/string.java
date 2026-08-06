395ab3efeb333bc8046d901bdc0ba182c1169934
lxakshaseth <lxakshatseth90@gmail.com>
2026-08-06 14:12:44 +0530

    Add Java String and Scanner examples

---
+++ String/string.java
@@ -0,0 +1,17 @@
+package String;
+
+import java.util.Scanner;
+
+
+public class string{
+    public static void main(String[] args) {
+        char arr [] = {'a' , 'b' , 'c', 'd'};
+        String str = "abcd";
+        String str2 = new String("xyz");
+
+        Scanner sc = new Scanner(System.in);
+        String name;
+        name = sc.nextLine();
+        System.out.println(name);
+    }
+}
