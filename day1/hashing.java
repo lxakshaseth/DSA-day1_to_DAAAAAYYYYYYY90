12260b1d4b12a7a4ec11c7edac3b2de81f2db9b9
Akshat Gupta <lxakshatsethseth90@gmail.com>
2026-09-09 11:00:49 +0530

    correct HashMap class name in hashing example

---
+++ day1/hashing.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class hashing{
+    public static void main(String args[]){
+        //create
+        HashMap < String , Integer > map = new HashMap<>();
+        //insert
+        map.put("india", 100);
+        map.put("bhutan", 10);
+        map.put("usa", 105);
+        map.put("nepal", 14);
+        map.put("russia", 199);
+        map.put("pak", 104);
+        map.put("america", 80);
+
+        System.out.println(map);
+
+
+
+    }
+}
